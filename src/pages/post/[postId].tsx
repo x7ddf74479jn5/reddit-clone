2cde2865293772d3f8696346373f1a15bb9f9899
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-07-17 21:59:35 +0900

    🩹 chore

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -75,7 +75,11 @@ const PostPage: NextPage = () => {
             className="h-24 rounded-md border border-gray-200 p-2 pl-4 outline-none disabled:bg-gray-50"
             placeholder={session ? "What are you thoughts?" : "Please sign in to comment"}
           />
-          <button type="submit" className="rounded-full bg-red-500 p-3 font-semibold text-white disabled:bg-gray-200">
+          <button
+            disabled={!session}
+            type="submit"
+            className="rounded-full bg-red-500 p-3 font-semibold text-white disabled:bg-gray-200"
+          >
             Comment
           </button>
         </form>
