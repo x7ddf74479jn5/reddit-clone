c47f086ef2561976036f4d863c6a3fb32a43fffb
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-07-17 21:26:48 +0900

    ✨ feat: vote

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,3 +1,4 @@
+import { useMutation, useQuery } from "@apollo/client";
 import {
   ArrowDownIcon,
   ArrowUpIcon,
@@ -9,15 +10,74 @@ import {
 } from "@heroicons/react/outline";
 import { Jelly } from "@uiball/loaders";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import Timeago from "react-timeago";
 
 import { Avatar } from "@/components/Avatar";
+import { ADD_VOTE } from "@/graphql/mutations";
+import { GET_ALL_VOTES_BY_POST_ID } from "@/graphql/queries";
 
 type PostProps = {
   post: Post;
 };
 
 export const Post: React.FC<PostProps> = ({ post }) => {
+  const [isVote, setIsVote] = useState<boolean | undefined>(undefined);
+  const { data: session } = useSession();
+
+  const { data, loading } = useQuery(GET_ALL_VOTES_BY_POST_ID, {
+    variables: {
+      post_id: post?.id,
+    },
+  });
+
+  const [addVote] = useMutation(ADD_VOTE, {
+    refetchQueries: [GET_ALL_VOTES_BY_POST_ID, "getVotesByPostId"],
+  });
+
+  const handleVote = async (isUpvote: boolean) => {
+    if (!session) {
+      toast("❗️ You'll need to sign up to vote");
+      return;
+    }
+
+    if (isVote && isUpvote) return;
+    if (isVote === false && !isUpvote) return;
+
+    console.log("voting...", isUpvote);
+
+    await addVote({
+      variables: {
+        post_id: post.id,
+        username: session?.user?.name,
+        upvote: isUpvote,
+      },
+    });
+  };
+
+  useEffect(() => {
+    const votes: Vote[] = data?.getVotesByPostId;
+
+    const isVote = votes?.find((vote) => vote.username == session?.user?.name)?.upvote;
+
+    setIsVote(isVote);
+  }, [data]);
+
+  const displayVotes = (data: any) => {
+    const votes: Vote[] = data?.getVotesByPostId;
+    const displayNumber = votes?.reduce((total, vote) => (vote.upvote ? (total += 1) : (total -= 1)), 0);
+
+    if (votes?.length === 0) return 0;
+
+    if (displayNumber === 0) {
+      return votes[0]?.upvote ? 1 : -1;
+    }
+
+    return displayNumber;
+  };
+
   if (!post) {
     return (
       <div className="flex w-full items-center justify-center p-10 text-xl">
@@ -30,9 +90,15 @@ export const Post: React.FC<PostProps> = ({ post }) => {
     <Link href={`/post/${post.id}`}>
       <div className="flex cursor-pointer rounded-md border border-gray-300 bg-white shadow-sm hover:border hover:border-gray-600 ">
         <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md bg-gray-50 p-4 text-gray-400">
-          <ArrowUpIcon className="voteButtons hover:text-red-400" />
-          <p className="text-sm font-bold text-black">0</p>
-          <ArrowDownIcon className="voteButtons hover:text-blue-400" />
+          <ArrowUpIcon
+            onClick={() => handleVote(true)}
+            className={`voteButtons hover:text-red-400 ${isVote && "text-red-400"}`}
+          />
+          <p className="text-sm font-bold text-black">{displayVotes(data)}</p>
+          <ArrowDownIcon
+            onClick={() => handleVote(false)}
+            className={`voteButtons hover:text-blue-400 ${isVote === false && "text-blue-400"}`}
+          />
         </div>
 
         <div className="p-3 pb-1">
