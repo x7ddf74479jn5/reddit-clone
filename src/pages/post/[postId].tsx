d4141e6462bf66f918fcc5af9f84910ceb4db3cd
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-07-17 20:05:24 +0900

    ✨ feat: comment UI

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -5,7 +5,9 @@ import { useSession } from "next-auth/react";
 import type { SubmitHandler } from "react-hook-form";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
+import TimeAgo from "react-timeago";
 
+import { Avatar } from "@/components/Avatar";
 import { Post } from "@/components/Post";
 import { ADD_COMMENT } from "@/graphql/mutations";
 import { GET_POST_BY_POST_ID } from "@/graphql/queries";
@@ -77,6 +79,26 @@ const PostPage: NextPage = () => {
             Comment
           </button>
         </form>
+      </div>
+
+      <div className="-my-5 rounded-b-md border border-t-0 border-gray-300 bg-white py-5 px-10">
+        <hr className="py-2" />
+        {post?.comments.map((comment) => (
+          <div className="relative flex items-center space-x-2 space-y-5" key={comment.id}>
+            <hr className="absolute top-10 left-7 z-0 h-16 border" />
+            <div>
+              <Avatar seed={comment.username} />
+            </div>
+
+            <div className="flex flex-col">
+              <p className="py-2 text-xs text-gray-400">
+                <span className="font-semibold text-gray-600">{comment.username}</span> -{" "}
+                <TimeAgo date={comment.created_at} />
+              </p>
+              <p>{comment.text}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
