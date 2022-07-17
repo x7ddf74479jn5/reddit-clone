59f176815ec19c5c30a2e47293ce7c2df26e41a9
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-07-17 18:49:38 +0900

    ✨ feat: comemnt mutation

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -1,5 +1,17 @@
 import { gql } from "@apollo/client";
 
+export const ADD_COMMENT = gql`
+  mutation MyMutation($post_id: ID!, $username: String!, $text: String!) {
+    insertComment(post_id: $post_id, username: $username, text: $text) {
+      created_at
+      id
+      post_id
+      text
+      username
+    }
+  }
+`;
+
 export const ADD_POST = gql`
   mutation MyMutation($body: String!, $image: String!, $subreddit_id: ID!, $title: String!, $username: String!) {
     insertPost(image: $image, body: $body, subreddit_id: $subreddit_id, title: $title, username: $username) {
